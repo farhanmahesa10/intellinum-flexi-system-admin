6571b7408bb948c261471cb0d9cf8cd8ff5cde3b
M Farhan <farhanmahesa10@gmail.com>
2023-11-23 11:31:09 +0700

    system-admin test company vision

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,42 @@
+//@ts-ignore
+import { PageLayout, themeToken } from "@intellinum/flexa-util";
+import { ConfigProvider, Layout, Space } from "antd";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <ConfigProvider theme={{ token: themeToken }}>
+      <Router>
+        <App />
+      </Router>
+    </ConfigProvider>
+  );
 }
+const { Header, Footer, Sider, Content } = Layout;
+const App = () => {
+  const navigate = useNavigate();
+  return (
+    <PageLayout
+      navigate={navigate}
+      pageContent={
+        <>
+          <Layout>
+            <Content style={{ minHeight: 280 }}>
+              <Routes>
+                <Route
+                  path="/system/company-vision"
+                  element={<>This is company vision flexa admin system</>}
+                />
+              </Routes>
+            </Content>
+          </Layout>
+        </>
+      }
+    />
+  );
+};
