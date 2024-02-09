2961878d55bb78bd64479cbaceec6ea8fe1715c2
quadratnp <quadratnp@gmail.com>
2024-02-09 14:23:25 +0700

    add new page custom integration

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -3,4 +3,12 @@ import Workflow from "./Workflow";
 import BusinessEvent from "./BusinessEvent";
 import EventHandler from "./EventHandler";
 import EventLog from "./EventLog";
-export { EventHandler, CompanyVision, Workflow, BusinessEvent, EventLog };
+import Custom from "./Custom";
+export {
+  EventHandler,
+  CompanyVision,
+  Custom,
+  Workflow,
+  BusinessEvent,
+  EventLog,
+};
