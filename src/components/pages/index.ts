15cc1de442a7ae87076b74c4840b258a0639ccbd
quadratnp <quadratnp@gmail.com>
2024-02-12 16:21:17 +0700

    add custom menu

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,6 +1,5 @@
 import TabMenu from "./TabMenu";
-import Custom from "./Custom";
 import { MqttTopic } from "./MqttTopic";
 import { MqttRule } from "./MqttRule";
 
-export { Custom, TabMenu, MqttTopic, MqttRule };
+export { TabMenu, MqttTopic, MqttRule };
